abe675737313d2bfceebe1c5cc0e7e6728f5a296
renard <jacob.arbib@gmail.com>
2020-09-07 16:11:37 +0200

    fsdfsdf

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class OpenLink : MonoBehaviour, IPointerClickHandler
+{
+    private TMP_Text pTextMeshPro;
+
+    private Camera pCamera;
+
+    private void Awake()
+    {
+        pTextMeshPro = GetComponent<TextMeshProUGUI>();
+        pCamera = Camera.main;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(pTextMeshPro, Input.mousePosition, pCamera);
+        if (linkIndex != -1)
+        { // was a link clicked?
+            TMP_LinkInfo linkInfo = pTextMeshPro.textInfo.linkInfo[linkIndex];
+
+            // open the link id as a url, which is the metadata we added in the text field
+            Application.OpenURL(linkInfo.GetLinkID());
+        }
+    }
+}
